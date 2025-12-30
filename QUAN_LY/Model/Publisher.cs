7f81ffcfa56fc4d8b8cc4dd86f165842030473c1
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 19:22:07 +0700

    code class Publisher

---
+++ QUAN_LY/Model/Publisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    [Table("publisher")]
+    public class Publisher
+    {
+        [Key]
+        [Column("publisher_id")] 
+        public int Id { get; set; } // publisher_id
+
+        [Column("name")]
+        [Required] 
+        [StringLength(255)] 
+        public string Name { get; set; }
+
+        [Column("phone")]
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [Column("address")]
+        [StringLength(255)]
+        public string Address { get; set; }
+
+        [Column("is_active")]
+        public bool IsActive { get; set; }
+    }
+}
