24e38a28e2c1ff5d6cd3f9c300e8297f9f2f660b
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 18:31:10 +0700

    code class Order.cs

---
+++ QUAN_LY/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class Order
+    {
+        public int Id { get; set; } // order_id
+        public int AdminId { get; set; } // Ai bán
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+        public int Status { get; set; } // 0: Pending, 1: Paid, 2: Cancelled
+        public string Note { get; set; }
+    }
+}
+
