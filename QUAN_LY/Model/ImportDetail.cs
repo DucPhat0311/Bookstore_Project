5afe49aa995caad6970f3a22fb2f39969e30ef9e
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 19:11:26 +0700

    code file cs ImportDetail

---
+++ QUAN_LY/Model/ImportDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class ImportDetail
+    {
+        public int Id { get; set; } // import_detail_id
+        public int ImportId { get; set; }
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+        public decimal ImportPrice { get; set; } // Giá vốn
+    }
+}
