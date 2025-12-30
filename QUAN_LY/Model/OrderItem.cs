f4280337ae29fcb19b29a70053e140499eab8a89
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 19:13:53 +0700

    code class OrderItem

---
+++ QUAN_LY/Model/OrderItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class OrderItem
+    {
+        public int Id { get; set; } // order_item_id
+        public int OrderId { get; set; }
+        public int BookId { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; } // Giá tại thời điểm bán
+    }
+}
