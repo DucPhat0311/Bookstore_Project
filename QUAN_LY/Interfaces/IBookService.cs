591897f39ae15fdb9e18989f98efde57ae77dcd3
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 18:49:52 +0700

    tao folder interfaces

---
+++ QUAN_LY/Interfaces/IBookService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using QUAN_LY.Model;
+
+namespace QUAN_LY.Interfaces
+{
+    public interface IBookService
+    {
+        List<Book> GetAllBooksForManagement(); // tiêu chí lấy tất cả sách của method ở trang quản lý sách khác với method ở POS (trang bán hàng)
+        List<Book> GetAllBooksForPOS(); // method này chỉ lấy sách chưa xóa và số lượng lớn hơn 0 khác với cái trên
+
+        Book GetBookById(int id); 
+        bool AddBook(Book book);
+        bool UpdateBook(Book book);
+        bool DeleteBook(int id);
+
+        List<Publisher> GetAllPublishers();
+
+        List<Book> SearchBooksForManagement(string keyword); // hàm tìm kiếm sách cho trang quản lý sách
+
+        List<Book> SearchBooksForPOS(string keyword); // hàm tìm kiếm sách cho trang POS
+
+
+    }
+}
