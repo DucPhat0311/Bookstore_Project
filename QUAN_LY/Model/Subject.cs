aa1a66c10a316660453e9c2b4329b41f143f5028
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 18:30:25 +0700

    code class Subject.cs

---
+++ QUAN_LY/Model/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class Subject
+    {
+        public int Id { get; set; } // subject_id
+        public string Name { get; set; }
+    }
+}
