a5ddd63816b6a52796ee0c803862d7dfaaa150dc
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 18:30:01 +0700

    code class Admin.cs

---
+++ QUAN_LY/Model/Admin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class Admin
+    {
+        public int Id { get; set; } // admin_id
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; } // password phải hash trước khi lưu
+        public string Role { get; set; } // "Manager" hoặc "Sale Staff"
+        public bool IsActive { get; set; }
+    }
+}
