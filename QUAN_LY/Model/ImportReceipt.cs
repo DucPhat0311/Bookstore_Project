8a0f1c79310e343d66cf0706566ff0fd7b2a2caf
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 19:11:45 +0700

    code file cs ImportReceipt

---
+++ QUAN_LY/Model/ImportReceipt.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class ImportReceipt
+    {
+        public int Id { get; set; } // import_id
+        public int PublisherId { get; set; }
+        public int AdminId { get; set; }
+        public DateTime ImportDate { get; set; }
+        public decimal TotalCost { get; set; }
+        public int Status { get; set; } // 0: Preparing, 1: Imported
+    }
+}
