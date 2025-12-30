f5ac782b21d12969bf477b641a0a4681e5b0f3cf
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-30 18:50:47 +0700

    code class Author.cs

---
+++ QUAN_LY/Model/Author.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Model
+{
+    public class Author
+    {
+        public int Id { get; set; } // author_id
+        public string Name { get; set; }
+    }
+}
