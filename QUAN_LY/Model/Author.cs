2335fc3e5ecc7763b9c5a44738d94440afe1c963
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-31 18:21:02 +0700

    map table va column tuong ung trong Author

--- QUAN_LY/Model/Author.cs
+++ QUAN_LY/Model/Author.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QUAN_LY.Model
 {
+    [Table("author")]
     public class Author
     {
+        [Key]
+        [Column("author_id")]
         public int Id { get; set; } // author_id
+
+        [Column("name")]
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
     }
 }
