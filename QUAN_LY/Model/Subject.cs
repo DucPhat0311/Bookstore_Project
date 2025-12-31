697569c156f7d6f341e69dc30a5fd88a10b47d6f
ducphat0311@gmail.com <ducphat0311@gmail.com>
2025-12-31 18:20:33 +0700

    map table va column tuong ung trong Subject

--- QUAN_LY/Model/Subject.cs
+++ QUAN_LY/Model/Subject.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QUAN_LY.Model
 {
+    [Table("subject")]
     public class Subject
     {
+        [Key]
+        [Column("subject_id")]
         public int Id { get; set; } // subject_id
+
+        [Column("name")]
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
     }
 }
