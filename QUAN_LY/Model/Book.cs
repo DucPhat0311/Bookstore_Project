a90fc36abdb01ed4ca5ce500e16c79b5c51541ee
ducphat0311@gmail.com <ducphat0311@gmail.com>
2026-01-02 19:29:19 +0700

    them validation cho cac thuoc tinh book + thuoc tinh lay duong dan cua image + thuoc tinh trang thai hang + thuoc tinh de join voi ten author

--- QUAN_LY/Model/Book.cs
+++ QUAN_LY/Model/Book.cs
@@ -1,32 +1,63 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using QUAN_LY.ViewModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 
 namespace QUAN_LY.Model
 {
+
     [Table("book")]
-    public class Book
+    public class Book : BaseViewModel
     {
         [Key]
-        [Column("book_id")] 
-        public int Id { get; set; }
+        [Column("book_id")]
+        public int Id { get; set; } 
 
+        private string _title;
         [Column("title")]
-        [Required] 
-        [StringLength(255)] 
-        public string Title { get; set; }
+        [Required(ErrorMessage = "Tên sách không được để trống")] 
+        [StringLength(255)]
+        public string Title
+        {
+            get => _title;
+            set { _title = value; OnPropertyChanged(); } 
+        }
 
+        private int _authorId;
         [Column("author_id")]
-        public int AuthorId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn Tác giả")] 
+        public int AuthorId
+        {
+            get => _authorId;
+            set { _authorId = value; OnPropertyChanged(); }
+        }
 
+        private int _subjectId;
         [Column("subject_id")]
-        public int SubjectId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn Thể loại")]
+        public int SubjectId
+        {
+            get => _subjectId;
+            set { _subjectId = value; OnPropertyChanged(); }
+        }
 
+        private int _publisherId;
         [Column("publisher_id")]
-        public int PublisherId { get; set; } 
+        [Range(1, int.MaxValue, ErrorMessage = "Vui lòng chọn NXB")]
+        public int PublisherId
+        {
+            get => _publisherId;
+            set { _publisherId = value; OnPropertyChanged(); }
+        }
 
+        private decimal _price;
         [Column("price")]
-        public decimal Price { get; set; }
+        [Range(typeof(decimal), "10000", "1000000", ErrorMessage = "Giá bán phải từ 1,000đ tới 1,000,000đ")]
+        public decimal Price
+        {
+            get => _price;
+            set { _price = value; OnPropertyChanged(); }
+        }
 
         [Column("quantity")]
         public int Quantity { get; set; }
@@ -67,6 +98,22 @@ namespace QUAN_LY.Model
                 return "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
             }
         }
+
+        public string StatusText
+        {
+            get
+            {
+                if (Quantity <= 0) return "Hết hàng";
+                if (Quantity < 10) return "Sắp hết";
+                return "Còn hàng";
+            }
+        }
+
+        [ForeignKey("AuthorId")]
+        public virtual Author Author { get; set; } 
+
+        public string AuthorName => Author?.Name ?? "Không rõ";
+
     }
     }
 
