964954af971cf09ad5888db46357013f871b6f1b
Vo Duong Thien <conik2203@gmail.com>
2026-01-04 00:46:40 +0700

    add IAdminService tam

---
+++ QUAN_LY/Interfaces/IAdminService.cs
@@ -0,0 +1,18 @@
+﻿// QUAN_LY/Interfaces/IAdminService.cs
+using QUAN_LY.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QUAN_LY.Interfaces
+{
+    public interface IAdminService
+    {
+        Task<List<Admin>> GetAllActiveAdminsAsync();
+        Task<Admin> GetAdminByIdAsync(int id);
+        Task<bool> AddAdminAsync(Admin admin);
+        Task<bool> UpdateAdminAsync(Admin admin);
+        Task<bool> DeactivateAdminAsync(int id); 
+        Task<List<Admin>> SearchAdminsAsync(string keyword);
+        Task<bool> IsUsernameExistsAsync(string username, int? excludeId = null);
+    }
+}
