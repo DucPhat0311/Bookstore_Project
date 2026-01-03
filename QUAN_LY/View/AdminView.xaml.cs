436d852187f27d32ed46a5ec45d530806fcc0cde
Vo Duong Thien <conik2203@gmail.com>
2026-01-04 02:55:55 +0700

    fix adminview

--- QUAN_LY/View/AdminView.xaml.cs
+++ QUAN_LY/View/AdminView.xaml.cs
@@ -1,5 +1,4 @@
-﻿// QUAN_LY/View/AdminView.xaml.cs
-using QUAN_LY.ViewModel;
+﻿using QUAN_LY.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -10,12 +9,29 @@ namespace QUAN_LY.View
         public AdminView()
         {
             InitializeComponent();
+            this.Loaded += AdminView_Loaded;
         }
+
+        private void AdminView_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is AdminViewModel vm)
+            {
+                vm.StatusMessage = "Sẵn sàng";
+            }
+        }
+
         private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
         {
             if (DataContext is AdminViewModel vm && vm.SelectedAdmin != null)
             {
-                vm.SelectedAdmin.Password = ((PasswordBox)sender).Password;
+                vm.PasswordInput = ((PasswordBox)sender).Password;
+
+                // GỌI VALIDATION THỦ CÔNG
+                if (vm.SelectedAdmin.AdminId == 0) // Thêm mới
+                {
+                    vm.SelectedAdmin.Password = vm.PasswordInput;
+                    vm.SelectedAdmin.ValidateProperty();
+                }
             }
         }
     }
