d411f2e4181909f71ce03050f9de72f79d78c073
Vo Duong Thien <conik2203@gmail.com>
2026-01-04 02:52:20 +0700

    add PasswordBoxBehavior

---
+++ QUAN_LY/Utilities/PasswordBoxBehavior.cs
@@ -0,0 +1,48 @@
+﻿// QUAN_LY/Utilities/PasswordBoxBehavior.cs
+using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace QUAN_LY.Utilities
+{
+    public class PasswordBoxBehavior : Behavior<PasswordBox>
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxBehavior),
+                new FrameworkPropertyMetadata(string.Empty, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault,
+                    OnPasswordPropertyChanged));
+
+        public string Password
+        {
+            get => (string)GetValue(PasswordProperty);
+            set => SetValue(PasswordProperty, value);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PasswordChanged += OnPasswordChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.PasswordChanged -= OnPasswordChanged;
+            base.OnDetaching();
+        }
+
+        private void OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = AssociatedObject.Password;
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var behavior = d as PasswordBoxBehavior;
+            if (behavior?.AssociatedObject != null && behavior.AssociatedObject.Password != (string)e.NewValue)
+            {
+                behavior.AssociatedObject.Password = (string)e.NewValue;
+            }
+        }
+    }
+}
