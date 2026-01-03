c2a951ae20e00d5d2e0935c8f05226fbda680687
Vo Duong Thien <conik2203@gmail.com>
2026-01-04 00:44:51 +0700

    update admin model

--- QUAN_LY/Model/Admin.cs
+++ QUAN_LY/Model/Admin.cs
@@ -1,18 +1,72 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// QUAN_LY/Model/Admin.cs
+using QUAN_LY.ViewModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace QUAN_LY.Model
 {
-    public class Admin
+    [Table("admin")]
+    public class Admin : BaseViewModel
     {
-        public int Id { get; set; } // admin_id
-        public string Name { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; } // password phải hash trước khi lưu
-        public string Role { get; set; } // "Manager" hoặc "Sale Staff"
-        public bool IsActive { get; set; }
+        [Key]
+        [Column("admin_id")]
+        public int AdminId { get; set; }
+
+        private string _name;
+        [Column("name")]
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [StringLength(100)]
+        public string Name
+        {
+            get => _name;
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        private string _username;
+        [Column("username")]
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống")]
+        [StringLength(100)]
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        private string _password;
+        [Column("password")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [MinLength(6, ErrorMessage = "Mật khẩu ít nhất 6 ký tự")]
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        private string _role;
+        [Column("role")]
+        [Required]
+        public string Role
+        {
+            get => _role;
+            set { _role = value; OnPropertyChanged(); }
+        }
+
+        private bool _isActive = true;
+        [Column("is_active")]
+        public bool IsActive
+        {
+            get => _isActive;
+            set { _isActive = value; OnPropertyChanged(); }
+        }
+
+        public string RoleDisplay => Role switch
+        {
+            "Super Admin" => "Quản trị hệ thống",
+            "Manager" => "Quản lý",
+            "Sale Staff" => "Nhân viên bán hàng",
+            _ => Role
+        };
+
+        public string StatusDisplay => IsActive ? "Đang làm việc" : "Đã nghỉ";
     }
 }
