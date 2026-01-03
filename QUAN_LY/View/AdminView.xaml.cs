4636bb5b76980bb92ef7074e785cd7c50d1c240a
Vo Duong Thien <conik2203@gmail.com>
2026-01-04 00:52:22 +0700

    add AdminView

---
+++ QUAN_LY/View/AdminView.xaml.cs
@@ -0,0 +1,22 @@
+﻿// QUAN_LY/View/AdminView.xaml.cs
+using QUAN_LY.ViewModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace QUAN_LY.View
+{
+    public partial class AdminView : UserControl
+    {
+        public AdminView()
+        {
+            InitializeComponent();
+        }
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is AdminViewModel vm && vm.SelectedAdmin != null)
+            {
+                vm.SelectedAdmin.Password = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
