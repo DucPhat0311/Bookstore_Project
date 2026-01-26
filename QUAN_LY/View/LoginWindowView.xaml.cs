adf50abf2a4864fa850a283a0c1eaeaa339f2f53
Vo Duong Thien <conik2203@gmail.com>
2026-01-27 01:55:32 +0700

    giao dien login moi

--- QUAN_LY/View/LoginWindowView.xaml.cs
+++ QUAN_LY/View/LoginWindowView.xaml.cs
@@ -1,35 +1,13 @@
-﻿using QUAN_LY.ViewModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Input;
 
 namespace QUAN_LY
 {
     public partial class LoginWindowView : Window
     {
-        private readonly LoginViewModel _viewModel;
-
         public LoginWindowView()
         {
             InitializeComponent();
-
-            
-            _viewModel = new LoginViewModel();
-            this.DataContext = _viewModel;
-
-            
-            txtUsername.Focus();
-        }
-
-        private void BtnLogin_Click(object sender, RoutedEventArgs e)
-        {
-           
-            _viewModel.Password = txtPassword.Password;
-
-          
-            if (_viewModel.LoginCommand.CanExecute(null))
-            {
-                _viewModel.LoginCommand.Execute(null);
-            }
         }
 
         private void BtnClose_Click(object sender, RoutedEventArgs e)
@@ -37,19 +15,11 @@ namespace QUAN_LY
             Application.Current.Shutdown();
         }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
-            if (e.LeftButton == MouseButtonState.Pressed)
-                this.DragMove();
-        }
-
-       
-        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.Key == Key.Enter)
-            {
-                BtnLogin_Click(sender, e);
-            }
+            base.OnMouseLeftButtonDown(e);
+            this.DragMove();
         }
     }
 }
