7d9f132501c124ffeb35c50d49fb675a460e1d87
Vo Duong Thien <conik2203@gmail.com>
2026-01-27 01:56:55 +0700

    thay link

--- QUAN_LY/App.xaml.cs
+++ QUAN_LY/App.xaml.cs
@@ -1,14 +1,57 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using QUAN_LY.Interfaces;
 using QUAN_LY.Model;
+using QUAN_LY.Services;
+using QUAN_LY.View;
+using QUAN_LY.ViewModel;
+using System;
+using System.Windows;
 
 namespace QUAN_LY
 {
     public partial class App : Application
     {
-        // Biến static để truy cập từ toàn bộ dự án: App.CurrentUser
+        public static new App Current => (App)Application.Current;
         public static Admin CurrentUser { get; set; }
+        public IServiceProvider Services { get; }
 
-        // Định nghĩa các Role cứng để tránh gõ sai chính tả sau này
+        public App()
+        {
+            Services = ConfigureServices();
+        }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            
+            services.AddDbContext<BookStoreDbContext>();
+
+            
+            services.AddSingleton<IAdminService, AdminServiceSQL>();
+            
+            services.AddTransient<LoginViewModel>();
+            services.AddTransient<AdminViewModel>();
+            services.AddTransient<PageModel>(); 
+
+           
+            services.AddTransient<LoginWindowView>();
+            services.AddTransient<MainWindowView>();
+
+            return services.BuildServiceProvider();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+           
+            var loginWindow = Services.GetRequiredService<LoginWindowView>();
+            
+            loginWindow.DataContext = Services.GetRequiredService<LoginViewModel>();
+            loginWindow.Show();
+        }
+
         public static class Roles
         {
             public const string SuperAdmin = "Super Admin";
